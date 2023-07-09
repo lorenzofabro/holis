27194d4a1802588bdb6b85240fc8b4a1c154bac5
Lorenzo Fabro <lorenzofabro@Lorenzos-MacBook-Pro.local>
2023-07-09 18:45:07 +0200

    🏁

--- src/routes/Message.svelte
+++ src/routes/Message.svelte
@@ -19,7 +19,7 @@
 						<P size="sm" style="text-align: end">{message.text}</P>
 					</div>
 					<div class="avatar" style="margin-left: 10px;">
-						<Avatar src={message.photoURL ?? ''} />
+						<Avatar src={message.photoUrl ?? ''} />
 					</div>
 				</div>
 			</Card>
@@ -31,7 +31,7 @@
 			>
 				<div class="flex-container">
 					<div class="avatar" style="margin-right: 10px;">
-						<Avatar src={message.photoURL ?? ''} />
+						<Avatar src={message.photoUrl ?? ''} />
 					</div>
 					<div class="message-container">
 						<P size="base"><strong>{message.name}</strong></P>
